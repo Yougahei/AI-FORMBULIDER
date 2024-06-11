113ef897a1e8e29886120551965df75fe846dfbd
18577447378 <dadg39@qq.com>
2024-06-11 17:32:46 +0800

    push

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { Router } from "next/router";
 import React, { useEffect } from "react";
 
 export default function Header() {
+  const router = useRouter();
   const path = usePathname();
   useEffect(() => {}, []);
   return (
     !path.includes("aiform") && (
       <div className="p-5 border-b shadow-sm">
         <div className="flex items-center justify-between">
-          <Button>Get Started</Button>
+          <Button onClick={() => router.push("/dashboard")}>Get Started</Button>
         </div>
       </div>
     )
